e9a1e935bbeb7c4e4b62f4e4cf74dd3f04a56c0d
JasmineE1308 <jasminechristo2@gmail.com>
2024-12-05 21:37:07 +0530

    Add files via upload

---
+++ ProductList.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import ProductCart from "./ProductCard";
+const ProductList = ({ addToCart }) => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+}, []);
+return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {products.map(product => (
+            <ProductCart key={product.id} product={product} addToCart={addToCart} />
+        ))}
+    </div>
+);
+};
+export default ProductList;
