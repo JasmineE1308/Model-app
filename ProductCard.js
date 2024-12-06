5330d0554cf786ae5fbda887e63c6c553c957ec2
JasmineE1308 <jasminechristo2@gmail.com>
2024-12-06 19:45:49 +0530

    Update ProductCard.js

--- ProductCard.js
+++ ProductCard.js
@@ -2,7 +2,8 @@ import React from 'react';
 const ProductCard = ({ product, addToCart })=> {
     return (
         <div className="border p-4 rounded-lg shadow-lg flex flex-col items-center">
-            <img src={product.thumbnailurl} alt={product.title} className="h-48 w-48 object-cover" />
+            {/* <img src={product.thumbnailurl} alt={product.title} className="h-48 w-48 object-cover" /> */}
+            <img src={product.image}className="h-48 w-48 object-cover"  alt='' />
             <h2 className="font-bold text-lg my-2">{product.title}</h2>
             <p className="text-lg text-green-600">${product.price}</p>
             <button
